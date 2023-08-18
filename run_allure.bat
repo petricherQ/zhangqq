4dbbed8be4f076edd414f61909faed59beb57615
petrichor <40196530+petricherQ@users.noreply.github.com>
2023-08-18 10:23:08 +0800

    Delete run_allure.bat

--- run_allure.bat
+++
@@ -1,4 +0,0 @@
-rmdir outputs\allure_report\allure_raw/s/q
-pytest --alluredir ./outputs/allure_report/allure_raw
-copy ".\config\environment.properties" ".\outputs\allure_report\allure_raw\"
-allure serve ./outputs/allure_report/allure_raw
